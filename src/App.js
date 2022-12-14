e0b4e89cdcd30e8ab41f3d3b101e52329e89636b
r-scott25 <rachelbernal2@gmail.com>
2022-12-14 17:05:01 -0600

    uncontrolled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useRef } from "react";
 
 function App() {
-  return <div className="App">
-    <h1>React App</h1>
-  </div>;
+  const txtTitle = useRef();
+  const hexColor = useRef();
+
+  console.log(txtTitle);
+  console.log(hexColor);
+
+  const submit = (e) => {
+    e.preventDefault();
+    const title = txtTitle.current.value;
+    const color = hexColor.current.value;
+    alert(`${title}, ${color}`);
+    txtTitle.current.value = "";
+    hexColor.current.value = "";
+  };
+  return (
+    <form onSubmit={submit}>
+      <input ref={txtTitle} type="text" placeholder="color title..." />
+      <input ref={hexColor} type="color" />
+      <button>ADD</button>
+    </form>
+  );
 }
 
 export default App;
