6ff396787490edb10d4e5dd669b7a67b39d144c1
r-scott25 <rachelbernal2@gmail.com>
2022-12-15 13:40:14 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import "./App.css";
 
 const tahoe_peaks = [
-  { name: "Freel", elevation: 10891},
-  { name: "Monuments", elevation: 10067},
-  { name: "Pyramid", elevation: 9983},
-  { name: "Tallac", elevation: 9735}
-]
+  { name: "Freel", elevation: 10891 },
+  { name: "Monuments", elevation: 10067 },
+  { name: "Pyramid", elevation: 9983 },
+  { name: "Tallac", elevation: 9735 },
+];
 
-function List({data, renderItem, renderEmpty}) {
-  return !data.length ? (renderEmpty) : 
-  (
-  <ul>
-    {data.map((item) => (
-      <li key={item.name}>
-        {renderItem(item)}
-      </li>
-    ))}
-  </ul>
+function List({ data, renderItem, renderEmpty }) {
+  return !data.length ? (
+    renderEmpty
+  ) : (
+    <ul>
+      {data.map((item) => (
+        <li key={item.name}>{renderItem(item)}</li>
+      ))}
+    </ul>
   );
 }
 
 function App() {
   return (
-   <List 
+    <List
       data={tahoe_peaks}
       renderEmpty={<p>This list is empty.</p>}
-      renderItem={item => <>{item.name} - {item.elevation} ft.</>}
+      renderItem={(item) => (
+        <>
+          {item.name} - {item.elevation} ft.
+        </>
+      )}
     />
   );
 }
