85785ba228416932dfaf8cadd924dd172600a366
r-scott25 <rachelbernal2@gmail.com>
2022-12-15 17:04:57 -0600

    nesting links

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
+import { Link, Outlet } from "react-router-dom";
 
 function Home() {
   return (
     <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
       <h1>My Website</h1>
     </div>
   );
@@ -11,7 +17,21 @@ function Home() {
 export function About() {
   return (
     <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
       <h1>About Us</h1>
+      <Outlet />
+    </div>
+  );
+}
+
+export function History() {
+  return (
+    <div>
+      <h1>Our History</h1>
     </div>
   );
 }
@@ -19,14 +39,16 @@ export function About() {
 export function Contact() {
   return (
     <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
       <h1>Contact Us</h1>
     </div>
-  )
+  );
 }
-
 
 export function App() {
   return <Home />;
 }
-
-
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { App, About, Contact } from './App';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { App, About, Contact, History } from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<App />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/contact" element={<Contact />} />
-  </Routes>
-  </BrowserRouter>,
-  document.getElementById('root')
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />}>
+        <Route path="history" element={<History />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
-
